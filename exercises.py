cbafb8ded275aafbdb6bd684a7d6cce75527343f
Lesparova Keleapere <malebogo.keleapere2@student.shu.ac.uk>
2026-04-19 19:59:32 +0000

    Initial commit

---
+++ exercises.py
@@ -0,0 +1,12 @@
+# 1. Using single quotes
+print('There’s a lot of it about.')
+
+# 2. Using double quotes
+print('"Certainly is", she said.')
+
+# 3. Mixing single and double quotes
+print('"She said \'Certainly is\'", Mel reported.')
+
+# 4. Using escape characters for backslash
+print('Left\\right')
+
